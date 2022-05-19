ededf5f4e8bd7e844d95f020b0d8190376977796
Rubylenshy <reztomoloju@gmail.com>
2022-05-19 06:22:38 +0100

    added sidebar.php

--- Group_37_inventory/includes/sidebar.php
+++ Group_37_inventory/includes/sidebar.php
@@ -1,3 +1,61 @@
 <?php
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+    
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+ 
+    <link href="../bootstrap/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="../bootstrap/sb-admin-2.css" rel="stylesheet">
+            
+        <div id="wrapper">
+
+           <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+                    <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                    </div>
+                    <div class="sidebar-brand-text mx-3">Cheffings Inventory</div>
+                </a>    
+                    <hr class="sidebar-divider my-0">
+                        <li class="nav-item">
+                        <a class="nav-link" href="index.php">
+                            <span style="font-weight: 700;">Home</span></a>
+                        </li>
+                        <hr class="sidebar-divider">
+                        <div class="sidebar-heading" style="font-size: 13px;">
+                        Tables
+                        </div>
+                        <!-- Tables Buttons -->
+
+                        <li class="nav-item">
+                        <a class="nav-link" href="orders.php">
+                            <span style="font-weight: 700;">Purchase Orders</span></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="recipes.php">
+                            <span style="font-weight: 700;">Recipes</span></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="menu.php">
+                            <span style="font-weight: 700;">Menu Cost</span></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="stocklevel.php">
+                            <span style="font-weight: 700;">Ingredient Stock Level</span></a>
+                        </li>
+                    </hr>
+            </ul>        
+        </div>        
+</html>
+        
+
